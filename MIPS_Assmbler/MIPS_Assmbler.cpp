ff4734814ccae504dbcb08773300fe21cd5f8ca3
JoshLee0915 <jlee1@mtech.edu>
2015-03-25 16:12:51 -0600

    tested using structs for MIPS commands

--- MIPS_Assmbler/MIPS_Assmbler.cpp
+++ MIPS_Assmbler/MIPS_Assmbler.cpp
@@ -12,16 +12,52 @@
 
 using namespace std;
 
+
+union command
+{
+	struct 
+	{
+		unsigned func : 6;
+		unsigned shamt : 5;
+		unsigned rd : 5;
+		unsigned rt : 5;
+		unsigned rs : 5;
+		unsigned opcode : 6;
+	} rType;
+	struct
+	{
+		unsigned imm : 16;
+		unsigned rt : 5;
+		unsigned rs : 5;
+		unsigned opcode : 6;
+	}iType;
+	struct
+	{
+		unsigned address : 26;
+		unsigned opcode : 6;
+	}jType;
+	unsigned int cmd;
+};
+
 int main()
 {
+	command c;
+	c.rType.opcode = 0;
+	c.rType.rs = 16;
+	c.rType.rt = 0;
+	c.rType.rd = 8;
+	c.rType.shamt = 0;
+	c.rType.func = 0x20;
+	printf("%#010x", c);
+
 	string tmp;
 	string line;
 	string file = "test.txt";
 	FileIO* flIO = new FileIO();
 	flIO->openFile(file);
 	tmp = flIO->loadTxtFile();
-	cout << tmp;
-	flIO->writeTextFile("test2.txt", tmp);
+//	cout << tmp;
+//	flIO->writeTextFile("test2.txt", tmp);
 	delete flIO;
 	getline(cin, tmp);
 	return 0;
